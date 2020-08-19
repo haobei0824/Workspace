4f06da7f88314c94fb2cecac38f131d99a3aacf4
haobei <haobei0824@gmail.com>
2020-08-19 14:13:29 +0800

    code Node，add mongodb 02.js

---
+++ Front/Code/Node/02/02-mongodb.js
@@ -0,0 +1,9 @@
+// 安装好mongoDB之后，使用sudo mongod --config /usr/local/etc/mongod.conf 命令，启动数据库
+// 引入mongoose第三方模块 用来操作数据库
+const mongoose = require('mongoose');
+// 数据库连接
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
+    // 连接成功
+    .then(() => console.log('数据库连接成功'))
+    // 连接失败
+    .catch(err => console.log(err, '数据库连接失败'));
