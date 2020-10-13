040ec89295fa0eb5cca1e225c2d15288562a0268
haobei <haobei0824@gmail.com>
2020-10-13 14:06:25 +0800

    Code Swift: update InitProtocol

--- Code/SwiftLanguage/Protocols/InitializerRequirements.swift
+++ Code/SwiftLanguage/Protocols/InitializerRequirements.swift
@@ -9,10 +9,20 @@ import Foundation
 
 protocol InitProtocol {
     init()
+    init(withName name:String)
+    init(withName name:String, age:Int16)
 }
 
 class InitProtocolClass: InitProtocol {
+    required init(withName name: String, age: Int16) {
+        print("InitProtocolClass conform init(withName,age) of InitProtocol")
+    }
+    
+    required init(withName name: String) {
+        print("InitProtocolClass conform init(withName) of InitProtocol")
+    }
+    
     required init() {
-        print("InitProtocolClass conform InitProtocol")
+        print("InitProtocolClass conform init() of InitProtocol")
     }
 }
--- Code/SwiftLanguage/Protocols/main.swift
+++ Code/SwiftLanguage/Protocols/main.swift
@@ -10,4 +10,10 @@ import Foundation
 var initProtocolClass1 = InitProtocolClass()
 print(initProtocolClass1)
 
+var initProtocolClass2 = InitProtocolClass(withName: "123")
+print(initProtocolClass2)
+
+var initProtocolClass3 = InitProtocolClass(withName: "123", age: 13)
+print(initProtocolClass3)
+
 
