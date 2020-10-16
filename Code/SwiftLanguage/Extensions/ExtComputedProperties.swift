fdee329fa9e98d1665deadb1c37fbf52a0251df3
haobei <haobei0824@gmail.com>
2020-10-16 12:05:11 +0800

    Code Swift, add Extensions

---
+++ Code/SwiftLanguage/Extensions/ExtComputedProperties.swift
@@ -0,0 +1,12 @@
+//
+//  ExtComputedProperties.swift
+//  Extensions
+//
+//  Created by Csy on 2020/10/16.
+//
+
+import Foundation
+
+extension Double {
+    var km: Double {return self / 1000}
+}
---
+++ Code/SwiftLanguage/Extensions/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Extensions
+//
+//  Created by Csy on 2020/10/16.
+//
+
+import Foundation
+
+var myHeight = 1.68
+print("\(myHeight.km)" + "km")
+
+
