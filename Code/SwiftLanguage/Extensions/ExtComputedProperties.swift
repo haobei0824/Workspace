32f4a7667ae65664f71749f10ed8325b27ac037c
haobei <haobei0824@gmail.com>
2020-10-18 22:23:07 +0800

    Code Swift,  Extensions, add func, convenience init

--- Code/SwiftLanguage/Extensions/ExtComputedProperties.swift
+++ Code/SwiftLanguage/Extensions/ExtComputedProperties.swift
@@ -10,3 +10,14 @@ import Foundation
 extension Double {
     var km: Double {return self / 1000}
 }
+
+extension NSObject {
+    convenience init(withName name: String) {
+        self.init()
+        print("initWithName func extension in NSObject");
+    }
+    
+    func myName() -> Void {
+        print("myName func extension in NSObject ")
+    }
+}
--- Code/SwiftLanguage/Extensions/main.swift
+++ Code/SwiftLanguage/Extensions/main.swift
@@ -10,4 +10,8 @@ import Foundation
 var myHeight = 1.68
 print("\(myHeight.km)" + "km")
 
+var a = NSObject(withName: "123");
+a.myName()
+
+
 
