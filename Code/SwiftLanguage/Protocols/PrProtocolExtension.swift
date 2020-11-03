eb4bffd8f10f46bbadb3bd0727d1fb4cb27b4f2e
haobei <haobei0824@gmail.com>
2020-11-03 11:47:56 +0800

    Code Swift Protocols， add contionally protocol extension

--- Code/SwiftLanguage/Protocols/PrProtocolExtension.swift
+++ Code/SwiftLanguage/Protocols/PrProtocolExtension.swift
@@ -23,3 +23,16 @@ func testProtocolExtension() -> Void {
     print("textualDescription() impled in extension: " + obj.textualDescription)
 }
 
+extension Array: PrPEDescription where Element: PrPEDescription {
+    var textualDescription: String {
+        let itemsAsText = self.map { $0.textualDescription }
+        return "[" + itemsAsText.joined(separator: ", ") + "]"
+    }
+}
+
+func testProtocolContionalExtension() -> Void {
+    let objs = [NSObject(),NSObject()]
+    
+    print("Array contional implement textualDescription() in extension: " + objs.textualDescription)
+}
+
