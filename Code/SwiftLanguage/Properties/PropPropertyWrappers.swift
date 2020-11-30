2badf6ac29a9b4feb0b71f71bdd913c9a24478bb
haobei <haobei0824@gmail.com>
2020-11-30 11:52:53 +0800

    code Swift Property， add Property Wrappers init

--- Code/SwiftLanguage/Properties/PropPropertyWrappers.swift
+++ Code/SwiftLanguage/Properties/PropPropertyWrappers.swift
@@ -17,10 +17,52 @@ struct PropTwelveOrLess {
     }
 }
 
+@propertyWrapper
+struct PropSmallNumber {
+    private var maximum: Int
+    private var number: Int
+
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, maximum) }
+    }
+
+    init() {
+//        self.init(wrappedValue:0)
+        print("propertyWrapper PropSmallNumber init() ")
+        maximum = 12
+        number = 0
+    }
+    init(wrappedValue: Int) {
+//        self.init(wrappedValue: wrappedValue, maximum: 12)
+        print("propertyWrapper PropSmallNumber init(wrappedValue: Int)")
+        maximum = 12
+        number = min(wrappedValue, maximum)
+    }
+    init(wrappedValue: Int, maximum: Int) {
+        print("propertyWrapper PropSmallNumber init(wrappedValue: Int, maximum: Int)")
+        self.maximum = maximum
+        number = min(wrappedValue, maximum)
+    }
+}
+
 struct PropSmallRectangle {
     @PropTwelveOrLess var height: Int
     @PropTwelveOrLess var width: Int
+    @PropSmallNumber var height2: Int   // init()
+    @PropSmallNumber var height3: Int = 2       // init(wrappedValue: Int)
+    @PropSmallNumber(wrappedValue: 2, maximum: 5) var height4: Int  // init(wrappedValue: Int, maximum: Int)
+    @PropSmallNumber(maximum: 9) var height5: Int = 4   // init(wrappedValue: Int, maximum: Int)
+    // 编译不过
+//    @PropSmallNumber(wrappedValue: 2, maximum: 5) var height6: Int = 4
 }
+
+struct PropSmallRectangle2 {
+    @PropTwelveOrLess var height: Int
+    @PropTwelveOrLess var width: Int
+}
+
+
 
 func testPropertyWrappers() -> Void {
     var rectangle = PropSmallRectangle()
