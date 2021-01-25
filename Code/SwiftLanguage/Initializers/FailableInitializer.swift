abb2416ec262d627b4ad8c7d48772910e5c4f387
haobei0824 <haobei0824@gmail.com>
2021-01-25 14:21:54 +0800

    code Swift Initializer，update failable initializer

--- Code/SwiftLanguage/Initializers/FailableInitializer.swift
+++ Code/SwiftLanguage/Initializers/FailableInitializer.swift
@@ -40,6 +40,35 @@ enum InitTemperatureUnit2: Character {
     case kelvin = "K", celsius = "C", fahrenheit = "F"
 }
 
+class InitAutomaticallyNamedDocument: InitDocument {
+    override init() {
+        super.init()
+        self.name = "[Untitled]"
+    }
+    
+    override init(name: String) {
+        super.init()
+        if name.isEmpty {
+            self.name = "[Untitled]"
+        } else {
+            self.name = name
+        }
+    }
+}
+
+class InitAutomaticallyNamedDocument2: InitDocument {
+    override init() {
+        super.init()
+        self.name = "[Untitled]"
+    }
+    
+    override init?(name: String) {
+        super.init(name: name)
+        self.name = "123"   // 代码不会走到，提前终止
+    }
+}
+
+
 
 func testFailableInitializer() -> Void {
     let d = InitDocument(name: "")
@@ -50,4 +79,8 @@ func testFailableInitializer() -> Void {
     print(u2)
     var u3 = InitTemperatureUnit2.kelvin
     print(u3)
+    
+    var d2 = InitAutomaticallyNamedDocument2(name: "")
+    print(d2)
+    
 }
