d54394b5547c9118727de51e14b0f6481fc04f87
haobei0824 <haobei0824@gmail.com>
2021-03-08 14:33:16 +0800

    code Swift little project,  01-Products, init tableview

--- Code/SwiftLittleProjects/01-Products/ProductListViewController.swift
+++ Code/SwiftLittleProjects/01-Products/ProductListViewController.swift
@@ -9,5 +9,13 @@ import UIKit
 
 
 class ProductListViewController: UIViewController {
+    var tableView : UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView = UITableView(frame: CGRect(origin: CGPoint.zero, size: self.view.bounds.size), style: .plain)
+        self.tableView.backgroundColor = .blue;
+        self.view .addSubview(self.tableView)
+    }
 }
