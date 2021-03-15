c8135d3082f47c4f37812cafae72b45ddd805377
haobei0824 <haobei0824@gmail.com>
2021-03-15 11:46:20 +0800

    code Swift little projects, 01-Products, update about view controller

--- Code/SwiftLittleProjects/01-Products/AboutViewController.swift
+++ Code/SwiftLittleProjects/01-Products/AboutViewController.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    var fullImageView : UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.fullImageView = UIImageView()
+        self.fullImageView.image = UIImage(named: "header-contact")
+        self.view.addSubview(self.fullImageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let safeInsets = self.view.safeAreaInsets
+        self.fullImageView.frame = CGRect(x: safeInsets.left, y: safeInsets.top, width: (self.view.bounds.size.width - safeInsets.left - safeInsets.right), height: 250)
+    }
+    
     
 }
