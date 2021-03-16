bec22b767a879483d19ae30b0c533a0ff6ce3ffe
haobei0824 <haobei0824@gmail.com>
2021-03-16 14:28:26 +0800

    code Swift little projects, 02-Stopwatch, init

---
+++ Code/SwiftLittleProjects/02-Stopwatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  02-Stopwatch
+//
+//  Created by jiayou on 2021/3/16.
+//
+
+import UIKit
+/**
+ label1：展示总的运行时间
+ label2: 展示最近一次lap到现在的运行时间
+ Reset/Lap 按钮
+ Start/Stop 按钮
+ Lap列表
+ 
+ */
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
