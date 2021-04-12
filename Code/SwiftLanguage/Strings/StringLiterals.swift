f05f8ed39b9611b7dd31aa9bf2595ec6eca89492
haobei0824 <haobei0824@gmail.com>
2021-04-12 14:35:54 +0800

    code Swift Strings，init

---
+++ Code/SwiftLanguage/Strings/StringLiterals.swift
@@ -0,0 +1,9 @@
+//
+//  StringLiterals.swift
+//  Strings
+//
+//  Created by jiayou on 2021/4/12.
+//
+
+import Foundation
+
---
+++ Code/SwiftLanguage/Strings/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  Strings
+//
+//  Created by jiayou on 2021/4/12.
+//
+
+import Foundation
+
+let quotation =
+"""
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+print(quotation)
+
+// \表示不换行
+let softWrappedQuotation =
+"""
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop."
+"""
+print(softWrappedQuotation)
+
