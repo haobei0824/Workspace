230702092e175ab14713b2195f02bcc2c8b729f1
haobei0824 <haobei0824@gmail.com>
2021-04-20 14:40:20 +0800

    code Swift Strings，update access

--- Code/SwiftLanguage/Strings/StringLiterals.swift
+++ Code/SwiftLanguage/Strings/StringLiterals.swift
@@ -7,3 +7,19 @@
 
 import Foundation
 
+func access() -> Void {
+    let greeting = "Guten Tag!"
+    greeting[greeting.startIndex]
+    // G
+    greeting[greeting.index(before: greeting.endIndex)]
+    // !
+    greeting[greeting.index(after: greeting.startIndex)]
+    // u
+    let index = greeting.index(greeting.startIndex, offsetBy: 7)
+    greeting[index]
+    // a”
+    
+    for index in greeting.indices {
+        print("\(greeting[index]) ", terminator: "")
+    }
+}
