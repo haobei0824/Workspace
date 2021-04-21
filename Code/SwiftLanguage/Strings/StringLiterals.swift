f4d1724f597bbf9562fed02236949715a4714cb5
haobei0824 <haobei0824@gmail.com>
2021-04-21 14:14:36 +0800

    code Swift Strings，updateSubstrings、unicodeRepresentations

--- Code/SwiftLanguage/Strings/StringLiterals.swift
+++ Code/SwiftLanguage/Strings/StringLiterals.swift
@@ -23,3 +23,35 @@ func access() -> Void {
         print("\(greeting[index]) ", terminator: "")
     }
 }
+
+func substring() -> Void {
+    let greeting = "Hello, world!"
+    let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
+    let beginning = greeting[..<index]
+    // beginning is "Hello"
+
+    // Convert the result to a String for long-term storage.
+    let newString = String(beginning)
+    
+    print(newString)
+}
+
+func unicodeRepresentations() -> Void {
+    let dogString = "Dog‼🐶"
+    for codeUnit in dogString.utf8 {
+        print("\(codeUnit) ", terminator: "")
+    }
+    // Prints "68 111 103 226 128 188 240 159 144 182 
+    
+    for codeUnit in dogString.utf16 {
+        print("\(codeUnit) ", terminator: "")
+    }
+    // Prints "68 111 103 8252 55357 56374
+    
+    for scalar in dogString.unicodeScalars {
+        print("\(scalar.value) ", terminator: "")
+    }
+    print("")
+    // Prints "68 111 103 8252 128054 ”
+
+}
